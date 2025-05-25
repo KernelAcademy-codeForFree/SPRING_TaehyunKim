3e01891d01f5d82175807ef8d3b29090f7f4c87a
김태현 <xogusrla7@gmail.com>
2025-05-25 15:22:14 +0900

    Remove IdeaProjects, temp.md

--- IdeaProjects/Deck/src/DeckTest.java
+++
@@ -1,9 +0,0 @@
-public class DeckTest {
-    public static void main(String[] args) {
-        Deck deck = new Deck();
-        Dealer dealer = new Dealer(deck);
-        Game game = new Game(deck, dealer);
-
-        game.setupPlayers();
-    }
-}
--- IdeaProjects/Pattern/src/Strategy_Pattern/Strategy.java
+++
@@ -1,6 +0,0 @@
-package Strategy_Pattern;
-
-public interface Strategy {
-    public abstract Hand nextHand();
-    public abstract void study(boolean win);
-}
